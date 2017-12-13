e45fdd26df3db8e3559ba284332fac4c44553bc0
Anna Bosman <annar@cs.up.ac.za>
2017-12-13 18:27:02 +0200

    Pulled random walks into a separate file; more decoupling necessary!

--- XOR_all_metrics.py
+++ XOR_all_metrics.py
@@ -4,8 +4,8 @@
 import math
 import tensorflow as tf
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt  # Matplotlib is used to generate plots of data.
+import fla_metrics as fla
 
 np.random.seed(1)
 
@@ -74,148 +74,6 @@ def init_progressive_mask(mask):
     funcs = [lambda x: bounds - x, lambda x: x - bounds]
     return np.piecewise(random_nums, conds, funcs)  # return initialised random numbers
 
-
-def compute_s(epsilon, walk_diff):
-    """Computer the symbolic array {-1,0,1} for the FEM metrics."""
-    conds = [walk_diff < -epsilon, (walk_diff >= -epsilon) & (walk_diff <= epsilon), walk_diff > epsilon]
-    funcs = [-1, 0, 1]
-    return np.piecewise(walk_diff, conds, funcs)
-
-
-def compute_h(symbolic_s):
-    """Computer the entropy of the three-point objects in symbolic array {-1,0,1}."""
-    bin_count = [0., 0., 0., 0., 0., 0.]
-    p = 0
-    q = 1
-    num_symbols = len(symbolic_s)
-    while q < (num_symbols - 1):
-        if symbolic_s[p] != symbolic_s[q]:
-            if symbolic_s[p] == 0:
-                if (symbolic_s[q] == 1):
-                    bin_count[0] += 1
-                else:
-                    bin_count[1] += 1
-            if symbolic_s[p] == 1:
-                if (symbolic_s[q] == 0):
-                    bin_count[2] += 1
-                else:
-                    bin_count[3] += 1
-            if symbolic_s[p] == -1:
-                if (symbolic_s[q] == 0):
-                    bin_count[4] += 1
-                else:
-                    bin_count[5] += 1
-        p += 1
-        q += 1
-
-    entropy = 0.
-    for i in range(0, 5):
-        if (bin_count[i] != 0):
-            bin_count[i] = bin_count[i] / (num_symbols - 2.)
-            entropy -= bin_count[i] * math.log(bin_count[i], 6)
-    return entropy
-
-
-def is_flat(s):
-    """Given symbolic array S, return true is all symbols = 0, and false otherwise."""
-    for i in range(0, len(s)):
-        if s[i] != 0:
-            return False
-    return True
-
-
-def get_epsilon_star(walk_diff):
-    """Calculate epsilon* for the FEM metrics."""
-    eps_base = 10.
-    eps_step = 10.
-    eps = 0.
-    eps_order = 0.
-    not_found = True
-    # Quickly find the order:
-    while (not_found):
-        symbolic_s = compute_s(eps, walk_diff)
-        if is_flat(symbolic_s):
-            not_found = False
-            eps_step = eps_step / eps_base
-        else:
-            eps = eps_step
-            eps_order += 1
-            eps_step *= eps_base
-    small_step = 0.01 * (10 ** eps_order)
-    not_found = True
-    while (not_found):
-        symbolic_s = compute_s(eps, walk_diff)
-        if is_flat(symbolic_s):
-            if eps_step <= small_step:
-                not_found = False
-            else:
-                eps -= eps_step
-                eps_step /= eps_base
-                eps += eps_step
-        else:
-            eps += eps_step
-    return eps  # this is epsilon*
-
-
-def compute_fem(walk_diff):
-    """Calculate the FEM metric."""
-    eps_star = get_epsilon_star(walk_diff)
-    incr = 0.05 * eps_star
-    h_max = 0.
-    for i in np.arange(0., eps_star, incr):
-        cur_h = compute_h(compute_s(i, walk_diff))
-        if cur_h > h_max: h_max = cur_h
-    return h_max
-
-
-def scale_walk(walk):
-    """Scales the walk of arbitrary fitness range to [0,1]"""
-    min_f = min(walk)
-    max_f = max(walk)
-    diff = max_f - min_f
-    return (walk - min_f) / diff
-
-
-def compute_m1(walk, epsilon):
-    """Calculate the neutrality metric M1. Input: progressive random walk (fitness values scaled to [0,1])"""
-    m1 = 0.
-    len_3p_walk = len(walk) - 2
-    for i in range(0, len_3p_walk):
-        min_f = min(walk[i:i + 3])
-        max_f = max(walk[i:i + 3])
-        if max_f - min_f <= epsilon: m1 += 1.
-
-    return m1 / len_3p_walk
-
-
-def compute_m2(walk, epsilon):
-    """Calculate the neutrality metric M2. Input: progressive random walk (fitness values scaled to [0,1])"""
-    m2 = 0.
-    temp = 0.
-    len_3p_walk = len(walk) - 2
-    for i in range(0, len_3p_walk):
-        min_f = min(walk[i:i + 3])
-        max_f = max(walk[i:i + 3])
-        if max_f - min_f <= epsilon:  # is neutral
-            temp += 1.0
-        elif temp > 0:
-            if temp > m2: m2 = temp
-            temp = 0
-
-    return m2 / len_3p_walk
-
-def compute_grad(walk, n, step_size):
-    # (1) calculate the fitness differences:
-    err_diff = np.diff(walk)
-    # (2) calculate the difference between max and min fitness:
-    fit_diff = np.amax(walk) - np.amin(walk)
-    # (3) calculate the total manhattan distance between the bounds of the search space
-    manhattan_diff = n * 2 * bounds
-    scaled_step = step_size / manhattan_diff
-    # (4) calculate g(t) for t = 1..T
-    g_t = (err_diff / fit_diff) / scaled_step
-    # (5) calculate G_avg
-    return np.mean(np.absolute(g_t)), np.std(np.absolute(g_t))
 
 ##############################
 X_data = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
@@ -331,32 +189,17 @@ print("Completed Training")
 # COMPUTE GRADIENT MEASURE
 # (1) calculate the fitness differences:
 err_diff = np.diff(error_history_py, axis=0)
-# (2) calculate the difference between max and min fitness:
-fit_diff = np.amax(error_history_py, axis=0) - np.amin(error_history_py, axis=0)
-# (3) calculate the total manhattan distance between the bounds of the search space
-manhattan_diff = all_weights.shape[0] * 2 * bounds
-scaled_step = step_size / manhattan_diff
-# (4) calculate g(t) for t = 1..T
-g_t = (err_diff / fit_diff) / scaled_step
-# (5) calculate G_avg
-g_avg = np.mean(np.absolute(g_t), axis=0)
-g_dev = np.std(np.absolute(g_t), axis=0)
-print "Grad1: ", (g_avg, g_dev)
 
-x1, x2 = compute_grad(np.asarray(error_history_py)[:, 0], all_weights.shape[0], step_size)
+x1, x2 = fla.compute_grad(np.asarray(error_history_py)[:, 0], all_weights.shape[0], step_size, bounds)
 print "Grad2: ", x1, x2
 
 # COMPUTE RUGGEDNESS MEASURE
 print(err_diff[:, 0])
-ss = compute_s(0.1, err_diff[:, 0])
-print(ss)
-print(compute_h(ss))
-print(get_epsilon_star(err_diff[:, 0]))
-print "and FEM is: ", compute_fem(err_diff[:, 0])
+print "and FEM is: ", fla.compute_fem(err_diff[:, 0])
 
 # COMPUTE NEUTRALITY MEASURE
-print "Neutrality M1: ", compute_m1(scale_walk(np.asarray(error_history_py)[:, 0]), 1.0e-8)
-print "Neutrality M2: ", compute_m2(scale_walk(np.asarray(error_history_py)[:, 0]), 1.0e-8)
+print "Neutrality M1: ", fla.compute_m1(fla.scale_walk(np.asarray(error_history_py)[:, 0]), 1.0e-8)
+print "Neutrality M2: ", fla.compute_m2(fla.scale_walk(np.asarray(error_history_py)[:, 0]), 1.0e-8)
 
 # DRAW FIGURES
 fig = plt.figure()
---
+++ random_samplers.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+
+def random_step_tf(weights, step_size):
+    return np.add(weights, np.random.uniform(-step_size, step_size, weights.shape))
+
+
+def progressive_random_step_tf(weights, mask, step_size, bounds):
+    random_steps = np.random.uniform(0, step_size, weights.shape)
+    mask = bounds_check(weights, mask, random_steps * mask, bounds)
+    return np.add(weights, random_steps * mask), mask
+
+
+def progressive_manhattan_random_step_tf(weights, mask, step_size, bounds):
+    random_steps = np.zeros(weights.shape)
+    shape = weights.shape
+    random_steps = random_steps.flatten()
+    ind = np.random.choice(weights.size, size=1)
+    random_steps[ind] = step_size  # Manhattan: step size is constant
+    random_steps = random_steps.reshape(shape)
+    mask = bounds_check(weights, mask, random_steps * mask, bounds)
+    return np.add(weights, random_steps * mask), mask
+
+
+def progressive_mask_tf(shape): # turn into a generator?
+    start = np.ones(shape) * -1
+    mask = np.random.randint(0, 2, shape)
+    start = start ** mask
+    return start
+
+
+def bounds_check(inputs, mask, step, bounds):
+    conds = [np.absolute(inputs + step) > bounds, np.absolute(inputs + step) <= bounds]
+    funcs = [lambda mask: -mask, lambda mask: mask]
+    return np.piecewise(mask, conds, funcs)  # return mask
+
+
+def init_progressive_mask(mask, bounds):
+    random_nums = np.random.uniform(0, bounds, mask.shape)
+    conds = [mask == 1, mask == -1]
+    funcs = [lambda x: bounds - x, lambda x: x - bounds]
+    return np.piecewise(random_nums, conds, funcs)  # return initialised random numbers
+
+
+if __name__ == '__main__':
+    my_init = np.empty(5) # test
+    my_step = 0.1
+    my_bounds = 1
+
+    start = progressive_mask_tf(my_init.shape)
+    print "Start mask: ", start
+
+    my_init = init_progressive_mask(start, my_bounds)
+    print "Initial point: ", my_init
+
+    print "Next step, random: ", random_step_tf(my_init, my_step)
+    print "Next step + mask, progressive random: ", progressive_random_step_tf(my_init, start, my_step, my_bounds)
+    print "Next step + mask, progressive manhattan random: ", progressive_manhattan_random_step_tf(my_init, start, my_step, my_bounds)
