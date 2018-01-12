Fixed a division by zero bug in the gradient metrics

fla_metrics.py
@@ -100,6 +100,7 @@ def scale_walk(walk):
     min_f = min(walk)
     max_f = max(walk)
     diff = max_f - min_f
+    if diff == 0: return walk
     return (walk - min_f) / diff
 
 
@@ -143,9 +144,47 @@ def compute_grad(walk, n, step_size, bounds):
     manhattan_diff = n * 2 * bounds
     scaled_step = step_size / manhattan_diff
     # (4) calculate g(t) for t = 1..T
-    g_t = (err_diff / fit_diff) / scaled_step
+    g_t = np.zeros_like(err_diff)
+    if fit_diff != 0: g_t = (err_diff / fit_diff) / scaled_step
     # (5) calculate G_avg
     return np.mean(np.absolute(g_t)), np.std(np.absolute(g_t))
+
+
+# This method must be run after a simulation, to calculate the necessary metrics on the obtained walks
+def calculate_metrics(all_walks, dim, step_size, bounds):
+    # Work with ALL walks:
+    # (1) Gradients [NB: requires a Manhattan walk!]:
+    my_grad = calc_grad(all_walks, dim, step_size, bounds)
+    print("Average Gavg and Gdev: ", my_grad)  # each column corresponds to outputs per walk
+    # (2) Ruggedness [NB: requires a ]:
+    my_rugg = calc_fem(all_walks)
+    print("Average FEM: ", my_rugg)  # each column corresponds to outputs per walk
+    #print("Max FEM: ", np.amax(my_rugg, 0))
+    # (3) Neutrality:
+    my_neut1, my_neut2 = calc_ms(all_walks)
+    print("Average M1: ", my_neut1)  # each column corresponds to outputs per walk
+    print("Average M2: ", my_neut2)  # each column corresponds to outputs per walk
+    return my_grad, my_rugg, my_neut1, my_neut2
+
+
+def calc_grad(all_walks, dim, step_size, bounds):
+    # (1) Gradients [NB: requires a Manhattan walk!]:
+    my_grad = np.apply_along_axis(compute_grad, 1, all_walks, dim, step_size, bounds)
+    return np.average(my_grad, 0)
+
+
+def calc_fem(all_walks):
+    # (2) Ruggedness [NB: requires a progressive random walk]:
+    my_rugg = np.apply_along_axis(compute_fem, 1, np.diff(all_walks, axis=1))
+    return np.average(my_rugg, 0)
+
+
+def calc_ms(all_walks):
+    # (3) Neutrality [NB: requires a progressive random walk which does not cross the search space more than once]:
+    all_err_diff = np.diff(all_walks, axis=1)
+    my_neut1 = np.apply_along_axis(compute_m1, 1, all_err_diff, 1.0e-8)
+    my_neut2 = np.apply_along_axis(compute_m2, 1, all_err_diff, 1.0e-8)
+    return np.average(my_neut1, 0), np.average(my_neut2, 0)
 
 
 if __name__ == '__main__':